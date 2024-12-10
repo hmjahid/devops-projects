34a41a2497b8693c85a4ef2ddd44655e41829ec0
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2024-12-10 23:55:38 +0600

    Upadte

--- terraform/s3-localstack-terraform/main.tf
+++ terraform/s3-localstack-terraform/main.tf
@@ -1,21 +1,18 @@
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
+  access_key                  = "test" # LocalStack default
+  secret_key                  = "test" # LocalStack default
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true # Avoid STS calls to check the account ID
 
   endpoints {
-    s3 = "http://localhost:4566"
-    sts = "http://localhost:4566"
-  }
-
-  default_tags {
-    tags = {
-      Environment = "Local"
-    }
+    s3  = "http://localhost:4566" # LocalStack S3 endpoint
+    sts = "http://localhost:4566" # LocalStack STS endpoint
   }
 }
+
+
 
 resource "aws_s3_bucket" "buckets" {
   for_each = toset(["bucket1", "bucket2", "bucket3"])
